e309f6c0f123fdc8a77447ab9b99d478f3252ab2
David Jay <davidgljay@gmail.com>
2019-01-04 23:15:03 -0800

    Updating googleAuth firestore function.

--- functions/index.js
+++ functions/index.js
@@ -4,37 +4,26 @@ const fetch = require('node-fetch');
 const sendSms = require('./twilio');
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
- res.send("Hello from Firebase!");
-});
-
-exports.getToken = functions.https.onRequest((req, res) => {
+exports.googleAuth = functions.firestore.document('credentials/{userId}').onCreate((snap, context) => {
+  if (!snap.data().code) {
+    return false
+  }
   let url = 'https://www.googleapis.com/oauth2/v4/token?'
-  url += `code=${req.query.code}&`
+  url += `code=${snap.data().code}&`
   url += `client_id=${functions.config().google.client_id}&`
   url += `client_secret=${functions.config().google.client_secret}&`
   url += `redirect_uri=${encodeURIComponent('https://82adb641.ngrok.io/auth')}&`
   url += 'grant_type=authorization_code'
-  console.log(req.query.code);
-  console.log(url);
 
   return fetch(url, {method: 'POST'})
-  .then(response => Promise.all([response.status, response.text()]))
-  .then(([status, text]) => res.status(status).send(text))
-  .catch(err => res.status(500).send(err))
+  .then(response => response.json())
+  .then(json => {
+    snap.ref.set(json)
+  })
+  .catch(err => snap.ref.update({error: 'An error occurred! ' + err}))
 })
 
 exports.sendSms = functions.https.onRequest((req, res) => {
   const message = req.query.text;
   sendSms('+314-210-7659', message);
 })
-
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text;
-//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//   return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//     return res.redirect(303, snapshot.ref.toString());
-//   });
-// });
