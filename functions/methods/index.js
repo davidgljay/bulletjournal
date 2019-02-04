a86317c891167e99f6197b78d4782dd8b0e7d9d9
David Jay <davidgljay@gmail.com>
2019-02-03 23:59:44 -0800

    Adding incomingSMS to methods index.

--- functions/methods/index.js
+++ functions/methods/index.js
@@ -3,11 +3,13 @@ const checkForUsers = require('./checkForUsers')
 const verifyPhoneNumber = require('./verifyPhoneNumber')
 const initialQuestions = require('./initialQuestions')
 const messageUser = require('./messageUser')
+const incomingSMS = require('./incomingSMS')
 
 exports.default = {
   googleAuth,
   checkForUsers,
   verifyPhoneNumber,
   initialQuestions,
-  messageUser
+  messageUser,
+  incomingSMS
 }
