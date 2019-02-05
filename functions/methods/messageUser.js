9968cfa1a98e45d4f5e653988d95c4ef16611d23
David Jay <davidgljay@gmail.com>
2019-02-04 22:32:15 -0800

    Adding ref to messageuser.

--- functions/methods/messageUser.js
+++ functions/methods/messageUser.js
@@ -2,7 +2,7 @@ const db = require('../db')
 const {refreshTokenIfNeeded, appendItems} = require('../gsheets')
 const {getDate} = require('../date')
 
-module.exports = ({questions, spreadsheetId, phone}, id) => {
+module.exports = ({questions, spreadsheetId, phone}, id, ref) => {
   return db.collection('credentials').doc(id).get()
     .then(credentials => {
       const {refresh_token, access_token} = credentials.data()
@@ -10,6 +10,6 @@ module.exports = ({questions, spreadsheetId, phone}, id) => {
     })
     .then(() => collection('users').doc(id).update({index: 0}))
     .then(() => sms(phone, questions[0]))
-    .then(() => collection('queue').doc(id).delete())
+    .then(() => ref.delete())
     .catch(err => console.log('Error sending initial question to user', err))
 }
