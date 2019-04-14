cd97a5c3c3e94657a6cb5707bbb26c5b6fa3d2e5
David Jay <davidgljay@gmail.com>
2019-04-13 23:44:28 -0700

    Updating row when messaging user.

--- functions/methods/messageUser.js
+++ functions/methods/messageUser.js
@@ -3,14 +3,14 @@ const {refreshTokenIfNeeded, appendItems} = require('../gsheets')
 const {getDate} = require('../date')
 const sms = require('../twilio')
 
-module.exports = ({questions, spreadsheetId, phone, credId, userId}, id, ref) => {
+module.exports = ({questions, spreadsheetId, phone, credId, userId, row}, id, ref) => {
   return db.collection('credentials').doc(credId).get()
     .then(credentials => {
       const {refresh_token, access_token} = credentials.data()
       return refreshTokenIfNeeded(appendItems([[getDate()]], 'A1', spreadsheetId))(credId, refresh_token, access_token)
     })
     .then(() => {
-      return db.collection('users').doc(userId).update({index: 0})
+      return db.collection('users').doc(userId).update({index: 0, row: row + 1})
     })
     .then(() => sms(phone, questions[0]))
     .then(() => ref.delete())
